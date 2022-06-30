356a965878b4d538d47a2f7bfdc0b95f381d6db6
Hyperillion <AndyHyprillion@users.noreply.github.com>
2022-06-30 17:20:19 +0800

    update pics

---
+++ microwaveOven/sketch.js
@@ -0,0 +1,15 @@
+let microwave;
+
+function preload() {
+  microwave = loadImage('assets/microwave.png')
+}
+
+function setup() {
+  let cnv = createCanvas(990, 720);
+  cnv.parent("microwaveOven");
+}
+
+function draw() {
+  background(255)
+
+}
